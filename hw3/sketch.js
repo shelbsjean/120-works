d73d8920072c1a1389de9e75402582332eb007a3
Shelby Swanson <shelby.swanson@umontana.edu>
2017-09-27 15:06:07 -0600

    Add files via upload

--- hw3/sketch.js
+++ hw3/sketch.js
@@ -1,1 +1,15 @@
+// setup function
+function setup() {
 
+  //create a canvas
+  createCanvas( 600, 400 );
+
+  //add color to canvas
+  background( 'blue');
+
+}
+//add draw function
+function draw(){
+  //add hello world and add a position
+  text("Hello World", 40, 50);
+}
